4d395d4756091aa7e0aa8744c9a6daca7f0c4cbe
Tin Zeljar <tinzeljar@gmail.com>
2021-05-23 02:58:06 +0200

    Header component

--- vuedept/src/App.vue
+++ vuedept/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,7 +9,24 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Header from '@/components/Header.vue' // @ is an alias to /src
+
+@Component({
+  components: {
+    Header
+  }
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ vuedept/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+      <img alt="Dept logo" src="../assets/logo.svg">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<style scoped lang="scss">
+#header {
+    padding: 15px;
+    border-bottom: solid;
+}
+</style>
